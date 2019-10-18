af234a8703657d6d35b0efbaa44dec96b3cb5577
maddox-schaller <56449884+maddox-schaller@users.noreply.github.com>
2019-10-18 10:17:11 -0700

    assignment 1

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,15 @@
-// Student name:
-// This is where your first assignment goes!
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("cuddlefish");
+        System.out.println("Eevee");
+        System.out.println(10+10);
+        System.out.println("Joe mama");
+        System.out.println("no");
+        System.out.println("october 18th, 2019");
+    }
+}
